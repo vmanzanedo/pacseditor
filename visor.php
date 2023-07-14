f8c8667b7c41d585ffe2ce4ada551bc7917a5a17
Diego Barbera <diegobarbera@gmail.com>
2023-07-14 08:14:04 -0300

    Ajuste version

--- visor.php
+++ visor.php
@@ -255,7 +255,7 @@ thead input {
     <strong>Copyright &copy; 2022 <a href="https://pentalogic.tech">Pentalogic S.A.</a>.</strong>
     Todos los derechos reservados.
     <div class="float-right d-none d-sm-inline-block">
-      <b>PACS Editor </b> 20230325
+      <b>PACS Editor </b> 20230713
     </div>
   </footer>
 </div>
